9e1652513116752825017ea0104339f3ec6aa9e3
joel <joeljuliandurand@gmail.com>
2023-06-20 05:06:02 -0300

    .

--- components/Comic/Index.jsx
+++ components/Comic/Index.jsx
@@ -1,13 +1,14 @@
 import styles from "./Index.module.css"
 import Anchor from "../Anchor/Anchor"
 import Image from "next/image"
+
 function Comic({ data }) {
     return (
         <article key={data?.id} className={styles.marvel_container}>
             <h4 className={styles.marvel_title}>{data?.title}</h4>
             <Anchor path={`comic/${data?.id}`}>
                 {data?.images.length > 0 && data.images !== null && data.images !== undefined && data.images.map((image, index) => (
-                    <img key={index} width={380} height={500} className={styles.marvel_image} src={`${image.path}.${image.extension}`} alt={data.description} />))}
+                    <Image key={index} width={380} height={500} className={styles.marvel_image} src={`${image.path}.${image.extension}`} alt={data.description} />))}
             </Anchor>
             <p className={styles.marvel_description}>{`${data?.description?.substr(0, 65)}`}</p>
 
